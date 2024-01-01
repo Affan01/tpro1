18e69c7569820c02f1e68075edfc07e71f21228b
Affan01 <affanhussain954@gmail.com>
2024-01-01 19:29:12 +0500

    klll

---
+++ first.js
@@ -0,0 +1,167 @@
+"use strict";
+// //Question No 1:
+// let named: string = "Affan";
+// let age: number = 22;
+// let isStudent: boolean = true;
+// console.log(`Name: ${named}`);
+// console.log(`Age: ${age}`);
+// console.log(`Is a student: ${isStudent}`);
+// //Question No 2:
+// let num1: number = 10;
+// let num2: number = 5;
+// let add: number = num1 + num2;
+// let subtract: number = num1 - num2;
+// let multiply: number = num1 * num2;
+// let divide: number = num1 / num2;
+// console.log(`Addition: ${add}`);
+// console.log(`Subtraction: ${subtract}`);
+// console.log(`Multiplication: ${multiply}`);
+// console.log(`Division: ${divide}`);
+// //Question No 3:
+// let favoriteLanguage: string = "TypeScript";
+// console.log(`My favorite programming language is ${favoriteLanguage}.`);
+// var a:number=5*2+10/5+10-20%3;
+// console.log(a);
+// // Q NO 1: Calculate your age based on the current year and your birth year.
+// var currentYear: number = 2023; 
+// var birthYear: number = 2001;
+// var age: number = currentYear - birthYear;
+// console.log(`My age is: ${age} years`);
+// // Q NO 2:calculates the area of a rectangle using length and width variables.
+// var lengthh: number = 5; 
+// var width: number = 3; 
+// var areaRectangle: number = lengthh * width;
+// console.log(`The area of the rectangle is: ${areaRectangle}`);
+// // Q NO 3:calculates the area of a circle.
+// var radius: number = 4; 
+// var areaCircle: number = Math.PI * Math.pow(radius, 2);
+// console.log(`The area of the circle is: ${areaCircle}`);
+// // Q NO 4: calculates the area of the cube.
+// var sideLength: number = 6; 
+// var areaCube: number = 6 * Math.pow(sideLength, 2);
+// console.log(`The surface area of the cube is: ${areaCube}`);
+// // Q NO 5:converts a temperature from Fahrenheit to Celsius and vice versa using a variable.
+// var fahrenheitTemp: number = 98.6; 
+// var celsiusTemp: number = (fahrenheitTemp - 32) * 5/9;
+// console.log(`${fahrenheitTemp} degrees Fahrenheit is equal to ${celsiusTemp.toFixed(2)} degrees Celsius`);
+// // Q NO 6:Convert a given number of seconds into minutes and seconds using variables.
+// var totalSeconds: number = 120; 
+// var minutes: number = Math.floor(totalSeconds / 60);
+// var remainingSeconds: number = totalSeconds % 60;
+// console.log(`${totalSeconds} seconds is equal to ${minutes} minutes and ${remainingSeconds} seconds`);
+// // Q NO 7:calculates the percentage.
+// var totalMarks: number = 500; 
+// var obtainedMarks: number = 420; 
+// var percentage: number = (obtainedMarks / totalMarks) * 100;
+// console.log(`The percentage is: ${percentage.toFixed(2)}%`);
+// // Q NO 8: converts given number of days in to weeks and days such as 17 days = 2 weeks and 3 days.
+// var totalDays: number = 17;
+// var weeks: number = Math.floor(totalDays / 7);
+// var remainingDays: number = totalDays % 7;
+// console.log(`${totalDays} days is equal to ${weeks} weeks and ${remainingDays} days`);
+// //Increment and Decrement Operator:
+// // Question No 1:
+// let a = 2;
+// let b = ++a * 2; 
+// //Answer: b=6
+// //Question No2:
+// let x = 5;
+// let y = x-- + 2;
+// // Answer: y=7
+// //Question No 3:
+// let x = 3;
+// let y = ++x + x++ + ++x;
+// // Answer: y=14
+// //Question No 4:
+// let m = 2;
+// let n = ++m * m++ * --m;
+// //Answer:  n=27
+// //Question No 5:
+// let a = 3;
+// let b = 5;
+// let result = ++a + b-- - a++ + --b;
+// // Answer: result=8
+// //Question No 6:
+// let m = 2;
+// let n = 4;
+// let p = m++ + ++n - --m + n--;
+// //Answer: m=2,n=4,p=10
+// //Question No 7:
+// let a = 5;
+// let b = 3;
+// let c = 2;
+// let d = 7;
+// let result = ++a * (b-- + c) / --d;
+// //Answer: a=6, b=2, c=2, d=6 ,result=5
+// //Question No 8:
+// let m = 2;
+// let n = 3;
+// let o = 4;
+// let result = m++ * (--n + m) / (o-- - n);
+// //Answer: m=3, o=3, n=2, result=5
+//###########################################
+// 
+// var a:number=6;
+// // var length:number=8;
+// var area:number=6*Math.pow(a,2);
+// console.log(`result ${area}`);
+// // Q NO 4: calculates the area of the cube.
+// var sideLength: number = 6; 
+// var areaCube: number = 6 * Math.pow(sideLength, 2);
+// console.log(`The surface area of the cube is: ${areaCube}`);
+// // / // Q NO 5:converts a temperature from Fahrenheit to Celsius and vice versa using a variable.
+//  var fahrenheitTemp: number = 98; 
+//  var celsiusTemp: number = (fahrenheitTemp - 32) * 5/9;
+//  console.log(`${fahrenheitTemp} degrees Fahrenheit is equal to ${celsiusTemp.toFixed(2)} degrees Celsius`);
+// var a ;
+// var b=4;
+// console.log();
+// var totalDays: number = 17;
+// var weeks: number = Math.floor(totalDays / 7);
+// var remainingDays: number = totalDays % 7;
+// console.log(`${totalDays} days is equal to ${weeks} weeks and ${remainingDays} days`);
+// console.log(weeks);
+//Question No 4:
+// let m = 2;
+// let n = ++m * m++ * --m;
+// console.log(n);
+// if(2<1){
+//     console.log("rr");
+// }else{
+//     console.log("no");
+//     if(3==3){
+//         console.log("yes");
+//     }
+// }
+// var a:string="n";
+// switch (a){
+//     case "b":
+//         console.log("named");
+//         case "n":
+//             console.log("age");
+//             break;
+//             case "m":
+//                 console.log("qualication");// }
+// let ff:number=Affan(2,8);
+// console.log(ff);
+// var arrowwh= (c:number,d:number):number=>c+d;
+// var ll:number=arrowwh(6,9);
+// console.log(ll);
+// var totalf:string[]=["gh","rn","mh","ah","ah"]
+// var nom:number[]=[1,2,3,4,5];
+// nom.push(8);
+// nom.pop()
+// nom.shift()
+// nom.unshift(9)
+// nom.splice(1,1,6)
+// console.log(totalf[0],nom);
+// console.log(nom[1]);
+// if(nom[1]/2!=0){
+//     console.log("even");
+// }
+function Affan(a, b) {
+    let result = a + b;
+    console.log(result);
+    // return result;
+}
+Affan(2, 3);
